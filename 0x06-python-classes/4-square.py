b6f1bd21412e1e8f360d4d8f942481a698cfaecb
lesego12 <nkosilesego93@gmail.com>
2022-12-20 23:44:55 +0000

    Square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,9 +6,9 @@
 class Square:
     """Represent a square."""
 
-
     def __init__(self, size=0):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
